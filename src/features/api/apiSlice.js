f7a4cd1a22b35e45d8ef9902615eec34eb63bc48
geomukkath <geomukkath@yandex.com>
2022-12-07 19:31:58 +0530

    Successfully displayed todo list

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,11 +1,12 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-  reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUr: " http://localhost:3500/" }),
+  reducerPath: "apiSlice",
+  baseQuery: fetchBaseQuery({ baseUrl: " http://localhost:3500/" }),
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/todos",
+      responseHandler: (response) => response.text(),
     }),
     addTodo: builder.mutation({
       query: (todo) => ({
--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,44 +1,58 @@
 // add imports
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
-import { useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { useGetTodosQuery } from "../api/apiSlice";
 
 const TodoList = () => {
-    const [newTodo, setNewTodo] = useState('')
+  const [newTodo, setNewTodo] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //addTodo
-        setNewTodo('')
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //addTodo
+    setNewTodo("");
+  };
 
-    const newItemSection =
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="new-todo">Enter a new todo item</label>
-            <div className="new-todo">
-                <input
-                    type="text"
-                    id="new-todo"
-                    value={newTodo}
-                    onChange={(e) => setNewTodo(e.target.value)}
-                    placeholder="Enter new todo"
-                />
-            </div>
-            <button className="submit">
-                <FontAwesomeIcon icon={faUpload} />
-            </button>
-        </form>
+  const newItemSection = (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">Enter a new todo item</label>
+      <div className="new-todo">
+        <input
+          type="text"
+          id="new-todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+      </div>
+      <button className="submit">
+        <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </form>
+  );
 
+  let content;
+  // Define conditional content
 
-    let content;
-    // Define conditional content
+  const { data, error, isLoading, isSuccess } = useGetTodosQuery();
 
-    return (
-        <main>
-            <h1>Todo List</h1>
-            {newItemSection}
-            {content}
-        </main>
-    )
-}
-export default TodoList
+  return (
+    <main>
+      <h1>Todo List</h1>
+      {newItemSection}
+      {error && <p>An error occured</p>}
+      {console.log(error)}
+      {isLoading && <p>Loading...</p>}
+
+      {isSuccess && (
+        <>
+          {data &&
+            data.map((todo) => {
+              return <div key={todo.id}>{todo.title}</div>;
+            })}
+        </>
+      )}
+    </main>
+  );
+};
+export default TodoList;
